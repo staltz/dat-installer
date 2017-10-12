edb11f075c330d2ebaf66fc47c838d55ff8199c8
Andre Staltz <andre@staltz.com>
2017-10-12 19:44:36 +0300

    Use Dat-node in nodejs-project and in foreground

--- nodejs-assets/nodejs-project/main.js
+++ nodejs-assets/nodejs-project/main.js
@@ -1,12 +1,41 @@
-// Rename this sample file to main.js to use on your project.
-// The main.js file will be overwritten in updates/reinstalls.
+var rn_bridge = require("rn-bridge");
+var Dat = require("dat-node");
 
-var rn_bridge = require('rn-bridge');
+rn_bridge.channel.on("message", packet => {
+  var msg = JSON.parse(packet);
+  rn_bridge.channel.send("Background got packet from foreground: " + packet);
+  switch (msg.type) {
+    case "START_DAT":
+      startDat(msg);
+      break;
 
-// Echo every message received from react-native.
-rn_bridge.channel.on('message', (msg) => {
-  rn_bridge.channel.send(msg);
-} );
+    default:
+      break;
+  }
+});
 
-// Inform react-native node is initialized.
+function startDat(msg) {
+  Dat(
+    msg.storagePath,
+    {
+      key: msg.datKey
+    },
+    function(err, dat) {
+      if (err) {
+        rn_bridge.channel.send("ERROR: " + err);
+      } else {
+        dat.joinNetwork(function(err2) {
+          if (err) {
+            rn_bridge.channel.send("ERROR: " + err2);
+          } else {
+            rn_bridge.channel.send(
+              "There are " + dat.network.connected + " connected peers"
+            );
+          }
+        });
+      }
+    }
+  );
+}
+
 rn_bridge.channel.send("Node was initialized.");
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import xs, { Stream } from "xstream";
 import { h, ScreenSource } from "@cycle/native-screen";
 import { ReactElement } from "react";
 import { Platform, StyleSheet, Text, View } from "react-native";
+const RNFS = require("react-native-fs");
 
 const styles = StyleSheet.create({
   container: {
@@ -33,25 +34,26 @@ type Sinks = {
 };
 
 export default function main(sources: Sources): Sinks {
-  const vdom$ = xs.of(
+  const response$ = sources.nodejs.startWith("");
+
+  const vdom$ = response$.map(response =>
     h(View, { style: styles.container }, [
       h(Text, { style: styles.welcome }, "Dat Installer"),
-      h(
-        Text,
-        { style: styles.instructions },
-        "Double tap R to reload, or shake"
-      )
+      h(Text, { style: styles.instructions }, response)
     ])
   );
 
-  sources.nodejs.addListener({
-    next: x => {
-      console.log("Cycle app got from nodejs:", x);
-    }
+  const nodejsRequest$ = xs.of({
+    type: "START_DAT",
+    storagePath:
+      RNFS.ExternalStorageDirectoryPath +
+      "/dats/778f8d955175c92e4ced5e4f5563f69bfec0c86cc6f670352c457943666fe639",
+    datKey:
+      "dat://778f8d955175c92e4ced5e4f5563f69bfec0c86cc6f670352c457943666fe639"
   });
 
   return {
     screen: vdom$,
-    nodejs: xs.never()
+    nodejs: nodejsRequest$.map(req => JSON.stringify(req))
   };
 }
