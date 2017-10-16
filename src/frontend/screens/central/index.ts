8cca13274b490044db8122d727644ae96250f38d
Andre Staltz <andre@staltz.com>
2017-10-16 23:29:38 +0300

    Refactor central, split out view()

--- src/frontend/screens/central/index.ts
+++ src/frontend/screens/central/index.ts
@@ -1,58 +1,9 @@
 import xs, { Stream } from "xstream";
-import { h } from "@cycle/native-screen";
 import { HTTPSource, RequestOptions } from "@cycle/http";
 import { ScreenVNode, ScreensSource, Command } from "cycle-native-navigation";
 import { AppMetadata } from "../../../typings/messages";
-import { ReactElement } from "react";
-import { Platform, StyleSheet, Text, View, FlatList } from "react-native";
-import ActionButton from "react-native-action-button";
+import view from "./view";
 const RNFS = require("react-native-fs");
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    flexDirection: "column",
-    justifyContent: "flex-start",
-    alignItems: "stretch",
-    backgroundColor: "#F5FCFF"
-  },
-  list: {
-    alignSelf: "stretch"
-  },
-  listContent: {
-    flex: 1,
-    flexDirection: "column"
-  },
-  emptyList: {
-    flex: 1,
-    justifyContent: "flex-end",
-    paddingBottom: 100,
-    paddingRight: 25,
-    alignItems: "flex-end"
-  },
-  emptyListTitle: {
-    fontSize: 20,
-    textAlign: "center",
-    color: "#202020",
-    margin: 10
-  },
-  emptyListSubtitle: {
-    fontSize: 15,
-    textAlign: "center",
-    color: "#202020",
-    margin: 10
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: "center",
-    margin: 10
-  },
-  info: {
-    textAlign: "center",
-    color: "#333333",
-    marginBottom: 5
-  }
-});
 
 export type Sources = {
   screen: ScreensSource;
@@ -65,36 +16,7 @@ export type Sinks = {
   http: Stream<RequestOptions>;
 };
 
-const emptyListVDOM = h(View, { style: styles.emptyList }, [
-  h(Text, { style: styles.emptyListTitle }, "Install an Android app!"),
-  h(
-    Text,
-    { style: styles.emptyListSubtitle },
-    "Press this button to get started"
-  )
-]);
-
-function view(apps$: Stream<Array<AppMetadata>>): Stream<ScreenVNode> {
-  return apps$.map(apps => ({
-    screen: "DatInstaller.Central",
-    vdom: h(View, { style: styles.container }, [
-      apps.length === 0
-        ? emptyListVDOM
-        : h(FlatList, {
-            style: styles.list,
-            contentContainerStyle: styles.listContent,
-            data: apps,
-            keyExtractor: (item: AppMetadata) => item.key,
-            renderItem: ({ item }: { item: AppMetadata }) => h(Text, item.key)
-          }),
-      h(ActionButton, {
-        buttonColor: "rgb(25, 158, 51)"
-      })
-    ])
-  }));
-}
-
-export default function main(sources: Sources): Sinks {
+export default function central(sources: Sources): Sinks {
   const apps$ = sources.http
     .select("allApps")
     .flatten()
